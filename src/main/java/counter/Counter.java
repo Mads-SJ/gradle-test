0c938603b0e5fb0631599ea110b9a89c98e67ed0
Mads-SJ <mads.soenderstrup@gmail.com>
2023-03-26 09:53:31 +0200

    first commit

---
+++ src/main/java/counter/Counter.java
@@ -0,0 +1,21 @@
+package counter;
+
+public class Counter {
+    private int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void reset() {
+        count = 0;
+    }
+}
---
+++ src/test/java/counter/CounterTest.java
@@ -0,0 +1,39 @@
+package counter;
+
+import  org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CounterTest {
+    private Counter counter;
+
+    @BeforeEach
+    void setUp() {
+        counter = new Counter();
+    }
+
+    @Test
+    void constructorTC1() {
+        assertEquals(0, counter.getCount());
+    }
+
+    @Test
+    void incrementTC2() {
+        counter.increment();
+        assertEquals(1, counter.getCount());
+    }
+
+    @Test
+    void decrementTC3() {
+        counter.decrement();
+        assertEquals(-1, counter.getCount());
+    }
+
+    @Test
+    void resetTC4() {
+        counter.increment();
+        counter.reset();
+        assertEquals(0, counter.getCount());
+    }
+}
